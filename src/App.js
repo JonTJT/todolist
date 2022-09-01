d57561404fe21c41c959b3c5baa295eecdff9002
Vynorimas <jontanjunting@gmail.com>
2022-09-01 13:47:12 +0800

    Fixed checklist issue

--- src/App.js
+++ src/App.js
@@ -9,13 +9,23 @@ import './App.css';
 
 function App() {
   const [ toDoList, setToDoList ] = useState(data);
+  
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    console.log("hello")
+    setToDoList(mapped);
+  }
 
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle}/>
     </div>
   );
 }
+
+
 
 export default App;
